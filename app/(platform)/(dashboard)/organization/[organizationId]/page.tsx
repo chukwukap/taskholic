5b2b4cb1cc6537c899fea4d7ba9efe5dfe5679bc
chukwuka promise uba <promiseuba67@gmail.com>
2023-12-29 13:45:10 +0100

    Add drag and drop

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,20 @@
-const OrganizationIdPage = () => {
-  return <div>Organization Page</div>;
+import { Separator } from "@/components/ui/separator";
+import { Info } from "./_components/info";
+import BoardList from "./_components/board-list";
+import { Suspense } from "react";
+
+const OrganizationIdPage = async () => {
+  return (
+    <div className="w-full mb-20">
+      <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <Suspense fallback={<BoardList.Skeleton />}>
+          <BoardList />
+        </Suspense>
+      </div>
+    </div>
+  );
 };
 
 export default OrganizationIdPage;
